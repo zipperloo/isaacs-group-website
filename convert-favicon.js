b949fa1ee0845f45c09c65bda3d0f2d1406f9d12
zipperloo <elliotisaacs@gmail.com>
2025-12-11 11:27:30 +0000

    3 - new favion

--- convert-favicon.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// Since we don't have image processing libs, let's just copy the logo
-// and let the browser handle the display as white through CSS
-fs.copyFileSync('logo.png', 'favicon.ico');
-console.log('Favicon created');
